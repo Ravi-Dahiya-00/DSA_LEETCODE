eb10b4ae47c3617ac069e6a5e8292d6568de16f6
Ravi Yadav <ravidahiya9350@gmail.com>
2026-06-01 21:43:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (30.12%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int ncr(int n,int r){
-        long long result=1;
-        for(int i=0;i<r;i++){
-            result=result*(n-i);
-            result=result/(i+1);
+    vector<int> ncr(int n){
+        long long ans=1;
+        vector<int> ans_row;
+        ans_row.push_back(1);
+        for(int i=1;i<n;i++){
+            ans=ans*(n-i);
+            ans=ans/i;
+            ans_row.push_back(ans);
         }
-        return result;
+        return ans_row;
     }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        for(int i=0;i<numRows;i++){
-            vector<int> temp;
-            for(int j=0;j<=i;j++){
-                temp.push_back(ncr(i,j));
-            }
-            ans.push_back(temp);
+        for(int i=1;i<=numRows;i++){
+            ans.push_back(ncr(i));
         }
         return ans;
     }
 };
+
