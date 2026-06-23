48d8e2bfee44d3597691f9ba2f6a8366fea98427
Ravi Yadav <ravidahiya9350@gmail.com>
2026-06-23 23:41:26 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (65.08%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -3,7 +3,9 @@ public:
     int maxNumberOfBalloons(string text) {
         unordered_map<char,int> mpp;
         for(auto it:text){
+            if(it=='b' || it=='a' || it=='l' || it=='o' || it=='n'){
             mpp[it]++;
+            }
         }
         return min({mpp['b'],mpp['a'],mpp['l']/2,mpp['o']/2,mpp['n']});
     }
