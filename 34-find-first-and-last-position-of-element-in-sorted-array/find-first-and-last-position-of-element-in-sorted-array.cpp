801b3e6df9f711417adfdefb58d12f2470044b59
Ravi Yadav <ravidahiya9350@gmail.com>
2026-07-02 11:57:18 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (19.43%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
-    int lowerbound(vector<int>&  nums,int target,int n){
-        int low=0,high=n-1;
-        int ans=n;
+    int first(vector<int> nums,int target){
+        int low=0,high=nums.size()-1;
+        int ans=-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]>=target){
+            if(nums[mid]==target){
                 ans=mid;
                 high=mid-1;
             }
-            else{
+            else if(nums[mid]<target){
                 low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
         return ans;
     }
-    int upperbound(vector<int>& nums,int target,int n){
-        int low=0,high=n-1;
-        int ans=n;
+    int last(vector<int> nums,int target){
+        int low=0,high=nums.size()-1;
+        int ans=-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]>target){
+            if(nums[mid]==target){
                 ans=mid;
-                high=mid-1;
+                low=mid+1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
             }
             else{
-                low=mid+1;
+                high=mid-1;
             }
         }
         return ans;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        if(n==0) return {-1,-1};
-        int lb=lowerbound(nums,target,n);
-        if(lb==n || nums[lb]!=target) return {-1,-1};
-        int hb=upperbound(nums,target,n);
-        return {lb,hb-1};
+        int first=this->first(nums,target);
+        int last=this->last(nums,target);
+        return {first,last};
     }
 };
