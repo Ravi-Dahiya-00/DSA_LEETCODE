c0238aae9f57198848404011887358603409b436
Ravi Yadav <ravidahiya9350@gmail.com>
2026-07-06 14:04:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (87.00%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int low=0,high=x;
+        while(low<=high){
+            long long mid=(low+high)/2;
+            long long a=mid*mid;
+            if(a==x){
+                return mid;
+            }
+            else if(a>x){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return high;
     }
 };
