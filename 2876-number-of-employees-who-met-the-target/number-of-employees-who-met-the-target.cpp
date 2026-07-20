f2f0b6ba99e2243f10cbe42f968d7f5ca169d656
Ravi Yadav <ravidahiya9350@gmail.com>
2026-07-20 21:35:08 +0530

    Time: 0 ms (100.00%) | Memory: 24.4 MB (86.35%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int count=0;
+        int n=hours.size();
+        for(int i=0;i<n;i++){
+            if(target<=hours[i]) count++;
+        }
+        return count;
+    }
+};
