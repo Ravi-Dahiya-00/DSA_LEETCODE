b9d46d9080846a5be180614c45eccd4182eabfa9
Ravi Yadav <ravidahiya9350@gmail.com>
2026-07-20 20:11:26 +0530

    Time: 0 ms (100.00%) | Memory: 26.6 MB (42.55%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -4,9 +4,9 @@ public:
          string ans="";
         for(string s:words){
             int n=s.length();
-            bool is=false;
-            for(int i=0;i<n;i++){
-                if(i==n-1) is=true;
+            bool is=true;
+            for(int i=0;i<n/2;i++){
+                
                 if(s[i]!=s[n-i-1]){
                     is=false;
                     break;
