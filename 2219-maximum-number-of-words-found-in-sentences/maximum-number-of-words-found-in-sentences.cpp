3e11bb71b91cfc07fe894c7b68e3170321045cc4
Ravi Yadav <ravidahiya9350@gmail.com>
2026-07-20 20:33:12 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (14.99%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
@@ -3,11 +3,13 @@ public:
     int mostWordsFound(vector<string>& sentences) {
         int count=0;
         for(string s:sentences){
+            if (s.length() == 0) continue;
             int space=0;
             for(char c:s){
                 if(c==' ') space++;
             }
-            if(space>=count) count=space+1;
+            // if(space>count) count=space+1;
+            count = max(space + 1, count);
         }
         return count;
     }
