3c6532c4f93d36129adc6039aae95daaad6408ee
Ravi Yadav <ravidahiya9350@gmail.com>
2026-07-22 16:44:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (98.86%) - LeetSync

--- 3312-number-of-changing-keys/number-of-changing-keys.cpp
+++ 3312-number-of-changing-keys/number-of-changing-keys.cpp
@@ -4,7 +4,7 @@ public:
         int count=0;
         int n=s.length();
         for(int i=1;i<n;i++){
-            if(abs(s[i]-s[i-1])!=32 && s[i]!=s[i-1]){
+            if(tolower(s[i])!=tolower(s[i-1])){
                 count++;
             }
         }
