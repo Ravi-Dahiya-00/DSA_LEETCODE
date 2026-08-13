a10e206400745329c1a32bc08f5f9ea520ac4b3e
Ravi Yadav <ravidahiya9350@gmail.com>
2026-08-13 18:28:56 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (77.89%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    vector<int> getLeftMaxArray(vector<int>& height,int n){
-        vector<int> leftmax(n);
-        leftmax[0]=height[0];
-
-        for(int i=1;i<n;i++){
-            leftmax[i]=max(leftmax[i-1],height[i]);
-        }
-        return leftmax;
-    }
-
-    vector<int> getRightMaxArray(vector<int>& height,int n){
-        vector<int> rightmax(n);
-        rightmax[n-1]=height[n-1];
-
-        for(int i=n-2;i>=0;i--){
-            rightmax[i]=max(rightmax[i+1],height[i]);
-        }
-        return rightmax;
-    }
     int trap(vector<int>& height) {
         int ans=0;
         int n=height.size();
-        vector<int> leftMax=getLeftMaxArray(height,n);
-        vector<int> rightMax=getRightMaxArray(height,n);
-
-        for(int i=0;i<n;i++){
-            int h=min(leftMax[i],rightMax[i])-height[i];
-            ans+=h;
+      
+        int leftmax=0;
+        int rightmax=0;
+        int left=0, right=n-1;
+        while(left<=right){
+            if(height[left]<=height[right]){
+                if(leftmax>height[left]){
+                    ans+=leftmax-height[left];
+                }else{
+                    leftmax=height[left];
+                }
+                left++;
+            }
+            else{
+                if(rightmax>height[right]){
+                    ans+=rightmax-height[right];
+                }else{
+                    rightmax=height[right];
+                }
+                right--;
+            }
         }
         return ans;
     }
