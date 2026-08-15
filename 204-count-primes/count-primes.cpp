02b6ef331dc642b01339419f2a508ee79bada749
Ravi Yadav <ravidahiya9350@gmail.com>
2026-08-15 20:56:21 +0530

    Time: 206 ms (31.36%) | Memory: 12.6 MB (91.61%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -4,7 +4,7 @@ public:
         
         vector<bool> isprime(n+1,true);
         int count=0;
-        for(int i=2;i<n;i++){
+        for(int i=2;i*i<n;i++){
             if(isprime[i]==true){
                 for(int j=2;i*j<n;j++){
                     isprime[i*j]=false;
