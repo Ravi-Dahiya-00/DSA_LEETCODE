6f01105b99fdf8e2d2ddd586595d5a18c72f870b
Ravi Yadav <ravidahiya9350@gmail.com>
2026-08-16 22:11:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.21%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    double solve(double x,long long n){
-        if(n==0) return 1.0;
-        
-        if(n<0) return solve(1/x,-n);
-        if(n%2==0) return solve(x*x,n/2);
-        return x*solve(x*x,(n-1)/2);
+    double Pow(double x,long long n){
+        if(n==0) return 1;
+        double half=Pow(x,n/2);
+        double ans=(half*half);
+        if(n%2==1) ans*=x;
+        return ans;
+
     }
     double myPow(double x, int n) {
-        return solve(x,(long)n);   
+        long long exp=n;
+        if(n<0){
+            return Pow(1/x,-exp);
+        }
+        return Pow(x,exp);
     }
 };
